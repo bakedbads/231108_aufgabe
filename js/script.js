da28ff90ee0b774a1915dc3ac450d40976e5cfa0
Koi Gstrein <gst.koikoi@gmail.com>
2023-11-12 23:01:58 +0100

    2nd task works, added titles for tasks

--- js/script.js
+++ js/script.js
@@ -7,5 +7,15 @@ const text = 'Todos von ' + vorname;
 
 document.getElementById("title").innerHTML = text;
 
+// NEUE KLASSE FÜR 1 KINDELEMENT ZUORDNEN
+
 const secondLi = document.querySelector('ul').children[1];
 secondLi.classList.add('done');
+
+// NEUES LISTENITEM HINZUFÜGEN
+
+const ul = document.querySelector('ul');
+
+const newlistitem = 'Schlafen';
+
+ul.appendChild(document.createElement('li')).innerHTML = newlistitem;
