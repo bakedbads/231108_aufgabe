f513944ec68b60acd192b2a848b5c1bf237849e6
Koi Gstrein <gst.koikoi@gmail.com>
2023-11-12 23:01:07 +0100

    added title

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 
 let vorname = 'Koi';
 let text = 'Todos von' + vorname;
+const vorname = 'Koi';
+const text = 'Todos von ' + vorname;
+
+document.getElementById("title").innerHTML = text;
 
 const secondLi = document.querySelector('ul').children[1];
 secondLi.classList.add('done');
